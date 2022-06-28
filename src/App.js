46a29ddab498c2da7c1ddc10ab4b00824657d8f0
hmtan <tanhuimian@stee.stengg.com>
2022-06-28 14:45:16 +0800

    async

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 
 import { useDispatch, useSelector } from 'react-redux';
-import {calculateTotals} from './features/cart/cartSlice';
+import {calculateTotals, getCartItems} from './features/cart/cartSlice';
 import Modal from "./components/Modal";
 import { useEffect } from "react";
 
 
 function App() {
-  const { cartItems } = useSelector((state)=>state.cart);
+  const { cartItems ,isLoading } = useSelector((state)=>state.cart);
 
   const {isOpen} = useSelector((state)=>state.modal);
   const dispatch = useDispatch();
@@ -16,6 +16,16 @@ function App() {
   useEffect(() => {
     dispatch(calculateTotals());
   },[cartItems]);
+
+  useEffect(() => dispatch(getCartItems()),[]);
+
+  if(isLoading){
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (  
     <main>
